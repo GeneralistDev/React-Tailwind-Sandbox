d612956ee0dc2cd9f0c47b845dde33a38339e709
Daniel Parker <dparker.tech@gmail.com>
2019-11-20 10:36:47 -0500

    Tailwindcss config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+import "./styles.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1 className="text-red-600">Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
