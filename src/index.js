f0abbf9e2a2ae390d1bf37ec23497bfd14cea9ae
Daniel Parker <dparker.tech@gmail.com>
2020-01-24 15:41:11 +1100

    Add in some font awesome free icons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 import "./styles.scss";
 
 function App() {
+  debugger;
   return (
     <div className="App">
       <h1 className="text-red-600">Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
     </div>
   );
 }
